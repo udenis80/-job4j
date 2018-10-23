4df2c49b54a608912c8d3487df3e70c129a0af6b
Denis Uhanov <udenis80@mail.ru>
2018-10-23 23:44:23 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,3 +1,5 @@
+package ru.job4j.calculate;
+
 /**
 * Package for calculate task.
 *
@@ -5,10 +7,21 @@
 * @version $Id$
 * @since 0.1
 */
+	public class Calculate {
 
-package ru.job4j.calculate;
-public class Calculate {
+     /**
+     * Main.
+     * @param args - args.
+     */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+	}
+       /**
+       * Method echo.
+       * @param name Denis Ukhanov.
+       * @return Echo plus Denis Ukhanov.
+       */
+	public String echo(String name) {
+    		return "Echo, echo, echo : " + name;
 	}
 }
