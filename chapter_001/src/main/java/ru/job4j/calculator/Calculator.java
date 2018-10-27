4b42c219ea4f0413a83093d28e7cc222eeffb92c
Denis Uhanov <udenis80@mail.ru>
2018-10-27 19:41:10 +0300

    3.1. Элементарный калькулятор.
    Signed-off-by: Denis Uhanov <udenis80@mail.ru>

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,30 @@
+
+package ru.job4j.calculator;
+
+/**
+ * Package for calculator task.
+ *
+ * @author Denis Ukhanov (udenis80@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    double result;
+
+    public void add(double first, double second); {
+        this.result = first + second;
+    }
+
+    public void subtract(double first, double second); {
+        this.result = first - second;
+    }
+    public void div (double first, double second); {
+        this.result = first / second;
+    }
+    public void multiple(double first, double second); {
+        this.result = first * second;
+    }
+    public double getResult() {
+        return this.result;
+
+    }
