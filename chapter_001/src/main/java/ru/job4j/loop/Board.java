cdd954e0a35775908081a8649e57adc28a6e3088
Denis Uhanov <udenis80@mail.ru>
2018-11-07 09:56:11 +0300

    5.3. Построить шахматную доску в псевдографике.

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -12,11 +12,10 @@ public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                 // условие проверки, что писать пробел или X
-                // Выше в задании мы определили закономерность, когда нужно проставлять X
-                if ((i + j) % 2 == 0) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int sum = i + j;// Выше в задании мы определили закономерность, когда нужно проставлять X
+                if (sum % 2 == 0) {   // условие проверки, что писать пробел или X
                     screen.append("X");
                 } else {
                     screen.append(" ");
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -11,13 +11,24 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class BoardTest {
+
+@Test
+public void when3x3() {
+    Board board = new Board();
+    String rsl = board.paint(3, 5);
+    String ln = System.lineSeparator();
+    assertThat(rsl, is(
+            String.format("X X%s X %sX X%s X %sX X%s", ln, ln, ln, ln, ln)
+            )
+    );
+}
     @Test
-    public void when3x3() {
-        Board board = new Board();
-        String rsl = board.paint(3, 3);
-        String ln = System.lineSeparator();
-        assertThat(rsl, is(
-                String.format("X X%s X %sX X%s", ln, ln, ln)
+    public void when5x4() {
+        Board board1 = new Board();
+        String rsl1 = board1.paint(5, 4);
+        String ln1 = System.lineSeparator();
+        assertThat(rsl1, is(
+                String.format("X X X%s X X %sX X X%s X X %s", ln1, ln1, ln1, ln1)
                 )
         );
     }
