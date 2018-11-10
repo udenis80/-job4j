8571a41f0808db69255335cc0668ed126a0b7ddb
Denis Uhanov <udenis80@mail.ru>
2018-11-10 22:04:27 +0300

    6.1. Классический поиск перебором.

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+/**
+ * Package for calculate task.
+ *
+ * @author Denis Ukhanov (udenis80@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class FindLoop {
+    public int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
