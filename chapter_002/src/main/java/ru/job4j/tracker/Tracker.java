9b1ee392762faab85298b378d7583a3a98785a2f
Denis Uhanov <udenis80@mail.ru>
2018-12-15 09:52:01 +0300

    1. Общая задача на второй модуль.

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,67 @@
+package ru.job4j.tracker;
+
+import java.util.*;
+/**
+ * @version $Id$
+ * @since 0.1
+ */
+public class Tracker {
+
+    private final Item[] items = new Item[100];  //Массив для хранение заявок.
+    private int position = 0;  //Указатель ячейки для новой заявки.
+    private static final Random RN = new Random();
+
+      /**
+     * Метод реализаущий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    public Item[] getItems() {
+        Item[] result = new Item[this.position];
+        for (int i = 0; i != this.position; i++) {
+            result[i] = this.items[i];
+        }
+        return result;
+    }
+
+    public Item[] findById(String id) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getID().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt(100));
+    }
+
+    public void replace(String id, Item item) {
+
+    }
+
+    public void delete(String id) {
+
+    }
+
+    public Item findByName(String id) {
+
+    }
+    public String addComment() {
+
+    }
+
+}
