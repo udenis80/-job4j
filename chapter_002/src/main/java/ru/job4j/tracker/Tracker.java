cc830c1bc75b29ed7d11001ff1335767f34cdc67
Denis Uhanov <udenis80@mail.ru>
2018-12-15 09:52:57 +0300

    1. Общая задача на второй модуль.

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Tracker {
 
     private final Item[] items = new Item[100];  //Массив для хранение заявок.
-    private int position = 0;  //Указатель ячейки для новой заявки.
+    private int position = 0;
     private static final Random RN = new Random();
 
       /**
