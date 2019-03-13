42ffe2f9b1c9ea7584fb493be3cf288d536a1e7d
udenis80 <udenis80@mail.ru>
2019-03-13 19:03:37 +0300

    4. Используя шаблон проектирования - стратегию [#786]

--- chapter_002/src/main/java/ru/job4j/pseudo/StartPseudo.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/StartPseudo.java
@@ -2,7 +2,8 @@ package ru.job4j.pseudo;
 
 public class StartPseudo {
     public static void main(String[] args) {
+       Paint paint = new Paint();
         paint.draw(new Triangle());
+        paint.draw(new Square());
     }
-
 }
