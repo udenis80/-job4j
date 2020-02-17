d890725ead60b06c6d2d72db163043bb2d14189e
udenis80 <udenis80@mail.ru>
2020-02-18 00:09:05 +0300

    UsageArrayList

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,22 +1,19 @@
 package ru.job4j.search;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PhoneDictionary {
-    private List<Person> persons = new ArrayList<Person>();
+   // private List<Person> persons = new ArrayList<Person>();
 
-    public void add(Person person) {
-        this.persons.add(person);
-    }
+   // public void add(Person person) {
+  //      this.persons.add(person);
+  //  }
 
     /**
      * Вернуть список всех пользователей, который содержат key в любых полях.
      * @param key Ключ поиска.
      * @return Список подошедщих пользователей.
      */
-    public List<Person> find(String key) {
-        List<Person> result = new ArrayList<>();
-        return result;
-    }
+  //  public List<Person> find(String key) {
+   //     List<Person> result = new ArrayList<>();
+  //      return result;
+  //  }
 }
