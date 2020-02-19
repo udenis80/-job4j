dd38c8fc8c03c197d29b3195b6b456c6da978530
udenis80 <udenis80@mail.ru>
2020-02-19 23:16:02 +0300

    Phone Dictionary

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,19 +1,32 @@
 package ru.job4j.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class PhoneDictionary {
-   // private List<Person> persons = new ArrayList<Person>();
 
-   // public void add(Person person) {
-  //      this.persons.add(person);
-  //  }
+    private List<Person> persons = new ArrayList<Person>();
 
-    /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     * @param key Ключ поиска.
-     * @return Список подошедщих пользователей.
-     */
-  //  public List<Person> find(String key) {
-   //     List<Person> result = new ArrayList<>();
-  //      return result;
-  //  }
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+        /**
+         * Вернуть список всех пользователей, который содержат key в любых полях.
+         * @param key Ключ поиска.
+         * @return Список подошедщих пользователей.
+         */
+
+        public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+            for (int i = 0; i < persons.size(); i++) {
+                if (persons.get(i) != null && persons.get(i).getName().contains(key)
+                        || persons.get(i).getSurname().contains(key) || persons.get(i).getAddress().contains(key)
+                        || persons.get(i).getPhone().contains(key)) {
+                    result.add(persons.get(i));
+                }
+            }
+        return result;
+    }
 }
