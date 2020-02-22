a0b1b7f3be6d0f112411dc0d160db75b580984f8
udenis80 <udenis80@mail.ru>
2020-02-22 23:02:22 +0300

    Конвертация двумерного массива в ArrayList

--- chapter_003/src/main/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ chapter_003/src/main/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -21,7 +21,6 @@ public class ConvertList2ArrayTest {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 0, 0},
-
         };
         assertThat(result, is(expect));
     }
