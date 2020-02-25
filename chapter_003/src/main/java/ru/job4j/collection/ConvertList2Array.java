e56f2a46c4e8ceb149540a6e6e6b0613c95c3513
udenis80 <udenis80@mail.ru>
2020-02-25 22:47:15 +0300

    Конвертация ArrayList в двухмерный массив

--- chapter_003/src/main/java/ru/job4j/collection/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -3,21 +3,17 @@ package ru.job4j.collection;
 import java.util.List;
 
 public class ConvertList2Array {
-    public static int[][] toArray(List<Integer> list, int rows) {
-        int x = list.size();
-        while (x % rows != 0) {
-            x++;
-        }
-        int cells = x / rows;
-        int [][] array = new int[rows][cells];
-        int y = 0;
-        for (int i[] : array) {
-            int index = 0;
-            for (int j : i) {
-                if (y < list.size()) {
-                    i[index++] = list.get(y++);
-                }
+    public static int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        int[][] array = new int[groups][cells];
+        int row = 0, cell = 0;
+        for (Integer i : list) {
+            if (cell == cells) {
+                row++;
+                cell = 0;
             }
+            array[row][cell] = i;
+            cell++;
         }
         return array;
     }
--- chapter_003/src/main/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ chapter_003/src/main/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.collection;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,7 +7,6 @@ import java.util.Arrays;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-@Ignore
 public class ConvertList2ArrayTest {
     @Test
     public void when7ElementsThen9() {
