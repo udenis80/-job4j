d188bc2834bad0e82c90eee03dc8ee093d87e1a5
udenis80 <udenis80@mail.ru>
2020-02-29 01:01:58 +0300

    Идентичные тексты.

---
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = false;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String i : origin) {
+            check.add(i);
+        }
+        for (String j : text) {
+            if (check.contains(j))
+                rsl = true;
+            break;
+        }
+        return rsl;
+    }
+}
