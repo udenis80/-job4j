238d3f810deb5bfbaeeb6a32250ffb9d9b7fd91b
udenis80 <udenis80@mail.ru>
2020-03-01 23:30:00 +0300

    Map, HashMap

---
+++ chapter_003/src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("udenis80@mail.ru", "Uhanov Denis Vladimirovich");
+        map.put("uregina85@mail.ru", "Uhanova Regina Nikolaevna");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
