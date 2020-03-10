dfd415afbfd16871b0e6db1fc117856a4c334612
udenis80 <udenis80@mail.ru>
2020-03-10 18:47:31 +0300

    Компаратор строк

---
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        for (int i = 0; i < left.length() && i < right.length(); i++) {
+            result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0) {
+                break;
+            }
+        }
+        if (result == 0) {
+            result = Integer.compare(left.length(), right.length());
+        }
+        return result;
+    }
+}
