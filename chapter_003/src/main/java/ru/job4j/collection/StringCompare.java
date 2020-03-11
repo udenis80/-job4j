4999654c71a6671c241f20bb07d7cf0c55228e9c
udenis80 <udenis80@mail.ru>
2020-03-11 19:22:11 +0300

    компоратор для строк

--- chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,15 +6,15 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int result = 0;
-        for (int i = 0; i < left.length() && i < right.length(); i++) {
+        char[] leftChar = left.toCharArray();
+        char[] rightChar = right.toCharArray();
+        int minLength = Math.min(leftChar.length, rightChar.length);
+        for (int i = 0; i < minLength; i++) {
             result = Character.compare(left.charAt(i), right.charAt(i));
-            if (result != 0) {
+            if (leftChar[i] != rightChar[i]) {
                 break;
             }
         }
-      //  if (result == 0) {
-      //      result = Integer.compare(left.length(), right.length());
-      //  }
         return result != 0 ? result : Integer.compare(left.length(), right.length());
     }
 }
