f9e558f95a21ffa06fffb5a8bf1fe3f123ecb956
udenis80 <udenis80@mail.ru>
2020-03-12 22:12:41 +0300

    компоратор строк

--- chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_003/src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,10 +5,10 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
+        int minLength = Math.min(left.length(), right.length());
         int result = 0;
         char[] leftChar = left.toCharArray();
         char[] rightChar = right.toCharArray();
-        int minLength = Math.min(leftChar.length, rightChar.length);
         for (int i = 0; i < minLength; i++) {
             result = Character.compare(left.charAt(i), right.charAt(i));
             if (leftChar[i] != rightChar[i]) {
