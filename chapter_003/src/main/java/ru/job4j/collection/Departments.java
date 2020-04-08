b4f5521c30c0db21183de6c1817247eae5643a9a
udenis80 <udenis80@mail.ru>
2020-04-08 23:42:07 +0300

    переход

---
+++ chapter_003/src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        List<String> rsl = new ArrayList<>();
+        return rsl;  //12
+    }
+
+    public static void sortAsc(List<String> orgs) {
+    }
+
+    public static void sortDesc(List<String> orgs) {
+    }
+}
+
