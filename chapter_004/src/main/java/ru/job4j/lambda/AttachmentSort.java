4d84a2325525e8c85ea84f598ff8ccd350c9ed28
udenis80 <udenis80@mail.ru>
2020-04-12 00:33:39 +0300

    Анонимные классы

--- chapter_004/src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ chapter_004/src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -1,5 +1,6 @@
 package ru.job4j.lambda;
 
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -22,12 +23,12 @@ public class AttachmentSort {
         attachments.sort(comparatorSize);
         System.out.println(attachments);
 
-       Comparator comparatorName =  new Comparator() {
+       Comparator<Attachment> comparatorName =  new Comparator<Attachment>() {
            @Override
-           public int compare(Object o1, Object o2) {
-               Attachment left = (Attachment) o1;
-               Attachment right = (Attachment) o2;
-               return left.getName().compareTo(right.getName());
+           public int compare(Attachment o1, Attachment o2) {
+           //    Attachment left = (Attachment) o1;
+             //  Attachment right = (Attachment) o2;
+               return o1.getName().compareTo(o2.getName());
            }
        };
         attachments.sort(comparatorName);
