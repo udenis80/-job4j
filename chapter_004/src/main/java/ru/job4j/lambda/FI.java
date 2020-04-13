fc4aea825203cfc76ba546217d973c2418f72d69
udenis80 <udenis80@mail.ru>
2020-04-13 16:21:27 +0300

    Лямбда блок

--- chapter_004/src/main/java/ru/job4j/lambda/FI.java
+++ chapter_004/src/main/java/ru/job4j/lambda/FI.java
@@ -7,30 +7,17 @@ import java.util.Comparator;
 public class FI {
     public static void main(String[] args) {
         Attachment[] atts = {
-                new Attachment("imagination", 20),
+                new Attachment("image", 20),
                 new Attachment("great", 120),
                 new Attachment("image 2", 23)
         };
-        Comparator<Attachment> comparator = new Comparator<Attachment>() {
-            @Override
-            public int compare(Attachment a, Attachment b) {
-                return a.getName().compareTo(b.getName());
-            }
+         Comparator<Attachment> comparator = (a, b) -> {
+            System.out.println("Сравним " + b.getName().length() + " с " + a.getName().length());
+            return b.getName().length() - a.getName().length();
         };
         Arrays.sort(atts, comparator);
-        for (Attachment c: atts
-             ) {
-            System.out.println(c);
-        }
-        Comparator<Attachment> comparator1 = new Comparator<Attachment>() {
-            @Override
-            public int compare(Attachment a, Attachment b) {
-                return b.getName().length() - a.getName().length();
-            }
-        };
-        Arrays.sort(atts,comparator1);
         for (Attachment a : atts
-             ) {
+        ) {
             System.out.println(a);
         }
     }
