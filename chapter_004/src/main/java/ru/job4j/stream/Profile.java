9738792222a85eddf705cf4d90caf51b71af4329
udenis80 <udenis80@mail.ru>
2020-04-19 00:22:27 +0300

    Список адресов

---
+++ chapter_004/src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+public class Profile {
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+//    public Profile(Address address) {
+//        this.address = address;
+//    }
+
+}
