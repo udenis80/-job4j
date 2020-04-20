8e3e266cecf277467c9db24cde339a1528fd5342
udenis80 <udenis80@mail.ru>
2020-04-20 13:17:29 +0300

    Уникальность элементов и сортировка

--- chapter_004/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -1,12 +1,14 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
+  private static   Comparator<Address> byAddress = (o1, o2) -> o1.getCity().compareTo(o2.getCity());
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(Profile::getAddress).distinct()
+                .map(Profile::getAddress).sorted(byAddress).distinct()
                 .collect(Collectors.toList());
     }
 }
--- chapter_004/src/main/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_004/src/main/test/java/ru/job4j/stream/ProfilesTest.java
@@ -12,10 +12,14 @@ public class ProfilesTest {
     @Test
     public void profilesCollectTest() {
         List<Profile> profileses = Arrays.asList(
+                new Profile(new Address("Sochi", "Lenina", 25, 23)),
+                new Profile(new Address("Sochi", "Lenina", 25, 23)),
+                new Profile(new Address("Tomsk", "Rubina", 2, 7)),
                 new Profile(new Address("Moscow", "Lenina", 25, 100)),
-                new Profile(new Address("Moscow", "Lenina", 25, 100)),
-                new Profile(new Address("Sochi", "Lenina", 25, 23)));
+                new Profile(new Address("Moscow", "Lenina", 25, 100))
+        );
         List result = Profiles.collect(profileses);
-        assertThat(2, is(result.size()));
+        result.stream().forEach(System.out::println);
+        assertThat(3, is(result.size()));
     }
 }
