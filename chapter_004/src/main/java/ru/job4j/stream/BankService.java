659503c3d3947297173e9c27d991944350778aad
udenis80 <udenis80@mail.ru>
2020-04-26 12:52:38 +0300

    Optional в Stream API.

--- chapter_004/src/main/java/ru/job4j/stream/BankService.java
+++ chapter_004/src/main/java/ru/job4j/stream/BankService.java
@@ -10,24 +10,20 @@ public class BankService {
     }
 
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            List<Account> list = users.get(user);
-            if (!list.contains(account)) {
-                list.add(account);
-            }
+        if (findByPassport(passport).isPresent()) {
+            users.get(findByPassport(passport)).add(account);
         }
     }
 
-    public User findByPassport(String passport) {
-        return users.keySet().stream().filter(user -> user.getPassport().equals(passport)).findFirst().orElse(null);
+    public Optional<User> findByPassport(String passport) {
+        return users.keySet().stream().filter(user -> user.getPassport().equals(passport)).findFirst();
     }
 
-    public Account findByRequisite(String passport, String requisite) {
-        User user = findByPassport(passport);
-        Account find = null;
-        if (user != null) {
-            find = users.get(user).stream().filter(account -> account.getRequisite().equals(requisite)).findFirst().orElse(null);
+    public Optional<Account> findByRequisite(String passport, String requisite) {
+        Optional<User> user = findByPassport(passport);
+        Optional<Account> find = Optional.empty();
+        if (user.isPresent()) {
+            find = users.get(user).stream().filter(account -> account.getRequisite().equals(requisite)).findFirst();
         }
         return find;
     }
@@ -35,11 +31,11 @@ public class BankService {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String requisiteDest, double amount) {
         boolean result = false;
-        Account account = findByRequisite(srcPassport, srcRequisite);
-        Account outAccount = findByRequisite(destPassport, requisiteDest);
-        if (account != null && outAccount != null && account.getBalance() >= amount) {
-            account.setBalance((account.getBalance() - amount));
-            outAccount.setBalance(outAccount.getBalance() + amount);
+        Optional<Account> account = findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> outAccount = findByRequisite(destPassport, requisiteDest);
+        if (account.isPresent() && outAccount.isPresent() && account.get().getBalance() >= amount) {
+            account.get().setBalance((account.get().getBalance() - amount));
+            outAccount.get().setBalance(outAccount.get().getBalance() + amount);
             result = true;
         }
         return result;
